25cbe06b9b7bfb16f7d4570bfaa9a4446b62492a
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-10-03 18:00:06 -0300

    componentes

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -12,26 +12,8 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form action={{ route('site.contato') }} method="post">
-
-                    @csrf
-                    <input type="text" name="nome" placeholder="Nome" class="borda-preta">
-                    <br>
-                    <input type="text" name="telefone" placeholder="Telefone" class="borda-preta">
-                    <br>
-                    <input type="text" name="email" placeholder="E-mail" class="borda-preta">
-                    <br>
-                    <select name="motivo-contato" class="borda-preta">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="1">Dúvida</option>
-                        <option value="2">Elogio</option>
-                        <option value="3">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-preta">ENVIAR</button>
-                </form>
+                @component('site.layouts._components.form_contato')
+                @endcomponent
             </div>
         </div>  
     </div>
