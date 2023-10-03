fc953901cf3674efe71490e74bf55eb64d221c3d
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-10-03 09:43:08 -0300

    for

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -11,32 +11,21 @@
 {{-- @dd($fornecedores); --}}
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br/>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br/>
-    CNPJ: {{ $fornecedores[1]['CNPJ'] ?? 'Dado não foi preenchido' }}
-    <br/>
-    Telefone: ({{ $fornecedores[1]['ddd'] ?? '' }}) {{ $fornecedores[1]['telefone'] ?? '' }}
-    <br/>
 
-    @switch($fornecedores[1]['ddd'])
-        @case('51')
-            Porto Algre
-            @break
-
-        @case('21')
-            Rio de Janeiro
-            @break
-
-        @case('41')
-            Curitiba
-            @break
-
-        @default
-            Local não identificado
-
-    @endswitch
-
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br/>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br/>
+        CNPJ: {{ $fornecedores[$i]['CNPJ'] ?? 'Dado não foi preenchido' }}
+        <br/>
+        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+        <hr/>
+        
+    @endfor
 
 @endisset
+
+<br/>
+
+
